9e708313941d71b8e682718cc88c579bdc96fd19
shiikharrr <69209995+shiikharrr@users.noreply.github.com>
2026-01-03 21:35:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const bands = [
-  'The Bled',
-  'Say Anything',
-  'The Midway State',
-  'We Came as Romans',
-  'Counterparts',
-  'Oh, Sleeper',
-  'A Skylit Drive',
-  'Anywhere But Here',
-  'An Old Dog'
+	'The Plot in You',
+	'The Devil Wears Prada',
+	'Pierce the Veil',
+	'Norma Jean',
+	'The Bled', 
+	'Say Anything', 
+	'The Midway State', 
+	'We Came as Romans', 
+	'Counterparts', 
+	'Oh, Sleeper', 
+	'A Skylit Drive', 
+	'Anywhere But Here', 
+	'An Old Dog'
 ];
 
 const strip = s => s.replace(/^(?:the|an|a)\s+/i, '').trim();
